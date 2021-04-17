7090bc75662521c1c1abac31fda0b29d3bfa5966
luiz.moraes <luiz.moraes@DESKTOP-42BG1MG.Dlink>
2021-04-16 22:25:59 -0300

    ending module 4 relationship for many

--- projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaRelacionamentoClienteConta.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaRelacionamentoClienteConta.java
@@ -17,8 +17,8 @@ public class TestaRelacionamentoClienteConta {
 		conta.setId(1L);
 		
 		Cliente cliente = new Cliente();
-		cliente.setNome("Maria");
-		cliente.setEndereco("Rua do Rosário, 80");
+		cliente.setNome("Leonardo");
+		cliente.setEndereco("Rua do ouvidor, 60");
 		cliente.setProfissao("Professor");
 		cliente.setConta(conta);
 		
